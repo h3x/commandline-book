5edf0a7d51e975199d8238b7ca3a7fca576830cd
Adam Austin <aausti23@myune.edu.au>
2023-01-06 20:22:06 +1100

    adam

--- todo/todo.go
+++ todo/todo.go
@@ -23,13 +23,13 @@ func (l *List) Add(task string) {
 		Task:        task,
 		Done:        false,
 		CreatedAt:   time.Now(),
-		CompletedAt: time.time{},
+		CompletedAt: time.Time{},
 	}
 
     *l = append(*l, t)
 }
 
-func (*l List) Complete(i int) error {
+func (l *List) Complete(i int) error {
     ls := *l
     if i <= 0 || i >len(ls) {
         return fmt.Errorf("Item %d does not exist", i)
@@ -39,5 +39,36 @@ func (*l List) Complete(i int) error {
     ls[i-1].CompletedAt = time.Now()
 
     return nil
+}
+
+func (l *List) Delete(i int) error {
+    ls := *l
+    if (i <= 0 || i > len(ls)) {
+        return fmt.Errorf("Item %d does not exist", i)
+    }
+    *l = append(ls[:i-1], ls[i:]...)
+    return nil
+}
+
+func (l *List) Save(filename string) error {
+    js, err := json.Marshal(l)
+    if err != nil {
+        return err
+    }
+    return ioutil.WriteFile(filename, js, 0644)
+}
+
+func (l *List) Get(filename string) error {
+    file, err := ioutil.ReadFile(filename)
+    if err != nil {
+        if errors.Is(err, os.ErrNotExist) {
+            return nil
+        }
+        return err
+    }
+    if len(file) == 0 {
+        return nil
+    }
+    return json.Unmarshal(file, l)
 }
 
